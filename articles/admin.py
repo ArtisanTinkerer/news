891281b0427296ecc241382c409669b521ad0860
Mick Byrne <mick.byrne@splend.com>
2024-11-22 11:51:51 +0000

    Added ability to add Comments.

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from articles.models import Article
+from articles.models import Article, Comment
+
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    list_display = ('title', 'content', 'author')
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
